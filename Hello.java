7175429497871aa20215c14fb8c234f7d869767d
SereinR <98984783+SereinR@users.noreply.github.com>
2025-11-12 14:42:58 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-public class Hello {
+public class HelloX {
     public static void main(String[] args) {
-        System.out.println("About to crash...")
+        System.out.println("About to crash...");
         
     }
 }
